a76db6c5f33d74f0974043b9074f479cf68cb9ff
yeasinshamimdev <yeasinshamim7@gamil.com>
2025-08-20 08:17:08 +0600

    module 1 complete

---
+++ module 1/adjacency_matrix_directed_graph.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n, e;
+  cin >> n >> e;
+  int adj_mat[n][n];
+  memset(adj_mat, 0, sizeof(adj_mat));
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      if (i == j)
+        adj_mat[i][j] = 1;
+
+  for (int i = 0; i < e; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+    adj_mat[a][b] = 1;
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      cout << adj_mat[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
